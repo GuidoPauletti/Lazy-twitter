da2a071952f4435b4411f9e5fbc89013b06a3ea4
Guido Pauletti <guido13pauletti@gmail.com>
2023-03-05 11:26:59 -0300

    create post

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,20 @@
 from django.shortcuts import render
 from .models import Post
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
 
 
 class PostListView(ListView):
     model = Post
-
     ordering = ['-date_posted']
 
 class PostDetailView(DetailView):
     model = Post
 
+class PostCreateView(CreateView):
+    model = Post
+    fields = ['title', 'content']
+    success_url = '/'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
