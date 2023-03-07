ed7a439e9a4bc4d417cfeeb5bb163289aa97fa8b
Guido Pauletti <guido13pauletti@gmail.com>
2023-03-07 15:02:37 -0300

    pagination

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,22 @@
 from .models import Post
+from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 class PostListView(ListView):
     model = Post
     ordering = ['-date_posted']
+    paginate_by = 2
+
+class UserPostListView(ListView):
+    model = Post
+    template_name = 'blog/user_posts.html'
+    paginate_by = 2
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get("username"))
+        return Post.objects.filter(author=user).order_by('-date_posted')
 
 class PostDetailView(DetailView):
     model = Post
